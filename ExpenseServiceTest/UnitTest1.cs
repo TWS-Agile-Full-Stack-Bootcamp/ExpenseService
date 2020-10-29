7940ce13349fe5bcef5adcf562db498c18c581bf
Chen Xu <xuchen@thoughtworks.com>
2020-10-29 17:40:23 +0800

    [chenxu]: update code base

--- ExpenseServiceTest/UnitTest1.cs
+++ ExpenseServiceTest/UnitTest1.cs
@@ -1,15 +1,48 @@
+using ExpenseService;
+using Xunit;
+
 namespace ExpenseServiceTest
 {
-    using ExpenseService;
-    using Xunit;
-
-    public class UnitTest1
+    public class ExpenseServiceTest
     {
         [Fact]
-        public void Test1()
+        public void Should_return_internal_expense_type_if_project_is_internal()
         {
-            var class1 = new Class1();
-            Assert.NotNull(class1);
+            // given
+            // when
+            // then
+        }
+
+        [Fact]
+        public void Should_return_expense_type_A_if_project_is_external_and_name_is_project_A()
+        {
+            // given
+            // when
+            // then
+        }
+
+        [Fact]
+        public void Should_return_expense_type_B_if_project_is_external_and_name_is_project_B()
+        {
+            // given
+            // when
+            // then
+        }
+
+        [Fact]
+        public void Should_return_other_expense_type_if_project_is_external_and_has_other_name()
+        {
+            // given
+            // when
+            // then
+        }
+
+        [Fact]
+        public void Should_throw_unexpected_project_exception_if_project_is_invalid()
+        {
+            // given
+            // when
+            // then
         }
     }
 }
